841c3accaafd2b7d322ad675088dcb5326c04ad0
Peter Myers <peter@myers.ooo>
2024-09-19 12:09:05 -0400

    Coverage.

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,9 @@
 export default {
-  roots: ['<rootDir>/src', '<rootDir>/test'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+      '^.+\\.ts$': 'ts-jest',
   },
-  testRegex: '/test/.*.test.ts$',
+  testEnvironment: 'node',
+  testRegex: './test/.*\\.(test|spec)?\\.(js|ts)$',
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    "src/**/*.ts*"
-  ],
-  coverageThreshold: {
-    "global": {
-      "lines": 90,
-      "statements": 90
-    }
-  }
-};
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+}
